7d52ded6bbd9b015fca88e4d3b2d0b11fb58b93b
unknown <oscarechegaraym@gmail.com>
2023-04-03 16:22:35 -0500

    Se agregó el proyecto

---
+++ resources/assets/js/app.js
@@ -0,0 +1,12 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('content')
+<b-container>
+    <b-row align-h="center">
+        <b-col cols="8">
+            <b-card 
+            title="Inicio de sesion">
+            <b-alert show>
+                Por favor ingresa tus datos
+            </b-alert>
+    
+            <b-form method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
+                <b-form-group label="Correo electronico" label-for="email"
+                    description="We'll nnever share your email with anyone else.">
+                    <b-form-input id="email"
+                        type="email"
+                        name="email"
+                        required
+                        value="{{ old('email') }}" required autofocus
+                        placeholder="example@programacionymas.com">
+                    </b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Contraseña" label-for="passowrd"
+                    description="We'll nnever share your email with anyone else.">
+                    <b-form-input id="password"
+                        type="password"
+                        name="password"
+                        required
+                        value="{{ old('password') }}" required>
+                    </b-form-input>
+                </b-form-group>
+
+                <b-form-group>
+                    <b-form-checkbox name="remember" checked="true" 
+                        {{ old('remember') ? 'checked' : '' }}>
+                        
+                        Recordar sesión
+                    </b-form-checkbox>
+                </b-form-group>
+                
+           
+                    <b-button type="submit" variant="primary">
+                        Ingresar
+                    </b-button>
+
+                    <b-button href="{{ route('password.request') }}" variant="link">
+                        ¿Olvidaste tu contraseña?
+                    </b-button>
+            
+            </b-form>
+                
+            </b-card>
+        </b-col>
+    </b-row>
+</b-container>
+@endsection
